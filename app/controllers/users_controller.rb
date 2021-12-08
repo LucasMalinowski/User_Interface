8bab481eb9a967c648bd7df84f5f8854d529d7eb
Gabriel Augusto da Silva <gabriel.augusto2504@gmail.com>
2021-12-08 14:50:03 -0300

    add modal popup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,10 @@ class UsersController < ApplicationController
 
   # GET /users/1 or /users/1.json
   def show
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   # GET /users/new
@@ -39,7 +43,7 @@ class UsersController < ApplicationController
 
   # PATCH/PUT /users/1 or /users/1.json
   def update
-    
+
     respond_to do |format|
       if @user.update(user_params)
         format.html { redirect_to @user, notice: "User was successfully updated." }
